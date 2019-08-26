a6f4ff2a13b78999fa970359a21bfbfdb78d99d7
Arne Kvaleberg <arnekva@gmail.com>
2019-08-26 11:27:13 +0200

    Added additional stupid texts

--- js/wordoftherefresh.js
+++ js/wordoftherefresh.js
@@ -2,18 +2,17 @@ $(document).ready(function() {
   let fineQuotes = [
   "Noe kontroversielt utsagn, men Quizmaster har ikke alltid rett",
   "Hansa er overvurdert",
-  "Er det egentlig verdt 1 time kø for quiz?",
-  "10 slurker på hjerter!",
   "Magnus må drikke, shalalalala",
-  "Uno er det beste kortspillet, don't @ me",
   "Let's get this bread",
   "Det fineste med å studere i Bergen er at de fleste bergensere reiser bort for å studere i andre byer",
   "Noen som blir med på Silent etterpå eller?",
   "To crispy chicken og en dobbel cheeseburger, takk!",
-  "Husk å drikk vann! Så lenge det er blandet med minst 20%.",
-  "Drikk 10 slurker",
-  "Husk å ta med kortstokk!",
-  "Husk en kortstokk så tiden i kø går fortere!"
+  "Husk en kortstokk så tiden i kø går fortere!",
+  "Gründer-idè: Fastfood-restaurant rett utforbi Kronbar",
+  "Det eneste Kronbar mangler er en avtale med Pizzabakeren for salg av mat",
+  "Håper at folk faktisk leser disse, det tok meg en god halvtime å skrive koden for disse teite tekstene",
+  "Noen som joiner kø 17:45 eller? Orker ikke sitte på scenen igjen"
+
 ];
 
 let randomItem = fineQuotes[Math.floor(Math.random()*fineQuotes.length)];
