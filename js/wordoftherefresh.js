19c55b5ff6787022002359e7a5aa22aed43fd671
Arne Kvaleberg <arnekva@gmail.com>
2019-12-19 13:36:07 +0100

    Updated word script

--- js/wordoftherefresh.js
+++ js/wordoftherefresh.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
   let fineQuotes = [
   "Noe kontroversielt utsagn, men Quizmaster har ikke alltid rett",
-  "Hansa er overvurdert",
   "Magnus må drikke, shalalalala",
-  "Let's get this bread",
   "Det fineste med å studere i Bergen er at de fleste bergensere reiser bort for å studere i andre byer",
   "Noen som blir med på Silent etterpå eller?",
   "To crispy chicken og en dobbel cheeseburger, takk!",
@@ -11,11 +9,11 @@ $(document).ready(function() {
   "Gründer-idè: Fastfood-restaurant rett utforbi Kronbar",
   "Det eneste Kronbar mangler er en avtale med Pizzabakeren for salg av mat",
   "Håper at folk faktisk leser disse, det tok meg en god halvtime å skrive koden for disse teite tekstene",
-  "Noen som joiner kø 17:45 eller? Orker ikke sitte på scenen igjen"
-
+  "Noen som joiner kø 16:45 eller? Orker ikke sitte på scenen igjen",
+  "6 pils, 4 vodka redbull og 2 bulmers på komfort"
 ];
 
 let randomItem = fineQuotes[Math.floor(Math.random()*fineQuotes.length)];
 
-document.getElementById('enEllerAnnenTekst').innerHTML = randomItem;
+document.getElementById('enEllerAnnenTekst').innerText = randomItem;
 });
